a2b92ebfa3b1845b96f5ff02f650168421d7498f
yamkela2 <101877916+yamkela2@users.noreply.github.com>
2023-05-02 04:53:36 -0700

    Add files via upload

---
+++ DevineDining/Controllers/HomeController.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using WebAppRestaurantDemoApp.Models;
+using WebAppRestaurantDemoApp.Repositories;
+using WebAppRestaurantDemoApp.ViewModels;
+using Microsoft.AspNetCore.Mvc;
+
+namespace WebAppRestaurantDemoApp.Controllers
+{
+    public class HomeController : Controller
+    {
+        private readonly RestaurantDBEntities restaurantDBEntities;
+        public HomeController()
+        {
+            restaurantDBEntities = new RestaurantDBEntities();
+        }
+        // GET: Home
+        public ActionResult Index()
+        {
+            var objCustomerRepository = new CustomerRepository();
+            var objItemRepository = new ItemRepository();
+            var objPaymentRepository = new PaymentTypeRepository();
+            var objMultipleModels = new Tuple<IEnumerable<SelectListItem>, IEnumerable<SelectListItem>, IEnumerable<SelectListItem>>(
+                    objCustomerRepository.GetAllCustomers(), objItemRepository.GetAllItems(), objPaymentRepository.GetAllPaymentType());
+
+            return View(objMultipleModels);
+        }
+
+        [HttpGet]
+        
+        public JsonResult getItemUnitPrice(int itemId)
+        {
+            decimal UnitPrice = restaurantDBEntities.Items.Single(model => model.ItemId == itemId).ItemPrice;
+            return Json(UnitPrice, JsonRequestBehavior.AllowGet);
+        }
+
+        [HttpPost]
+        public JsonResult Index(OrderViewModel objOrderViewModel)
+        {
+            OrderRepository objOrderRepository = new OrderRepository();
+            bool isStatus = objOrderRepository.AddOrder(objOrderViewModel);
+            string SuccessMessage = String.Empty;
+
+            if (isStatus)
+            {
+                SuccessMessage = "Your Order Has Been Successfully Placed.";
+            }
+            else
+            {
+                SuccessMessage = "There Is Some Issue While Placing Order.";
+            }
+            return Json(SuccessMessage, JsonRequestBehavior.AllowGet);
+        }
+    }
+}
---
+++ DevineDining/ViewModels/OrderViewModel.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+
+namespace WebAppRestaurantDemoApp.ViewModels
+{
+    public class OrderViewModel
+    {
+        public int OrderId { get; set; }
+        public int PaymentTypeId { get; set; }
+        public int CustomerId { get; set; }
+        public string OrderNumber { get; set; }
+        public DateTime OrderDate { get { return DateTime.Now; } }
+        public decimal FinalTotal { get; set; }
+        public IEnumerable<OrderDetailViewModel> listOrderDetailViewModel { get; set; }
+    }
+}
